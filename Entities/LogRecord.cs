064e3d5d86920ac3281af555f81dfc731e634b86
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-05-30 22:08:09 -0300

    Exercicio Resolvido

---
+++ Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Exercicio.Entities
+{
+    internal class LogRecord
+    {
+        public string? UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string userName, DateTime instant)
+        {
+            UserName = userName;
+            Instant = instant;
+        }
+
+        public override int GetHashCode() => UserName.GetHashCode();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is not LogRecord)
+            {
+                return false;
+            }
+            LogRecord? other = obj as LogRecord;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Exercicio.Entities;
+using System;
+using System.IO;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> conjunto = new HashSet<LogRecord>();
+
+            Console.Write("Enter full path: ");
+            string? path = Console.ReadLine();
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vect = sr.ReadLine().Split(' ');
+                        string userName = vect[0];
+                        DateTime instant = DateTime.Parse(vect[1]);
+                        LogRecord logRecord = new(userName, instant);
+                        conjunto.Add(logRecord);
+                    }
+
+                    Console.WriteLine("Total users: " + conjunto.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred: " + e.Message);
+            }
+        }
+    }
+}
